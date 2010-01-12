9111a7634a8c8570f5f38eee66eb7df054df5503
Ryan McKillen <rmckillen@donovan-brays-macbook-pro-2.local>
2010-01-11 17:42:10 -0800

    removed backgroundrb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -24,8 +24,6 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem "httparty"
   config.gem "pauldix-feedzirra", :lib => 'feedzirra', :source => 'http://gems.github.com'
-  #config.gem "chronic"
-  #config.gem "packet"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- lib/workers/rss_worker.rb
+++
@@ -1,7 +0,0 @@
-class RssWorker < BackgrounDRb::MetaWorker
-  set_worker_name :rss_worker
-  def create(args = nil)
-    # this method is called, when worker is loaded for the first time
-  end
-end
-
