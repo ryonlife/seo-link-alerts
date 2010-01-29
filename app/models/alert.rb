20b818171fc64995ee2b0827838bf67459e4b847
Ryan McKillen <ryan.mckillen@gmail.com>
2010-01-28 21:33:39 -0800

    seomoz api

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,21 +1,23 @@
 class Alert < ActiveRecord::Base
   belongs_to :feed
-  after_create { self.send_later(:get_metrics) }
+  after_create { self.send_later(:get_metrics) } 
   
   def get_metrics
     require 'cgi'
     require 'base64'
     require 'open-uri'
-  
-    ACCESS_ID = CGI::escape('member-acf8d277df')
-    SECRET_KEY = CGI::escape('cf23f577e2d6bf7b22aa23e6cbaa6158')  
-    EXPIRES = Time.now.to_i + (60*60*2)
-    API_CALL = 'url-metrics'
-    URL = self.url
-  
-    signature = CGI::escape(Base64.encode64(HMAC::SHA1.digest(SECRET_KEY, "#{ACCESS_ID}\n#{EXPIRES}")).chomp)
-    to_fetch = "http://lsapi.seomoz.com/linkscape/#{API_CALL}/#{URL}?AccessID=#{ACCESS_ID}&Expires=#{EXPIRES}&Signature=#{signature}"
+    require 'hmac-sha1'
+    
+    access_id = CGI::escape('member-acf8d277df')
+    secret_key = CGI::escape('cf23f577e2d6bf7b22aa23e6cbaa6158')  
+    expires = Time.now.to_i + (60 * 60 * 2)
+    api_call = 'url-metrics'
+    url = self.url
+
+    signature = CGI::escape(Base64.encode64(HMAC::SHA1.digest(secret_key, "#{access_id}\n#{expires}")).chomp)
+    to_fetch = "http://lsapi.seomoz.com/linkscape/#{api_call}/#{url}?AccessID=#{access_id}&Expires=#{expires}&Signature=#{signature}"
     self.metrics = open(to_fetch).read
-    self.save
+    # self.save
   end
+  
 end
