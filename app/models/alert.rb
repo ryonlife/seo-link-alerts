3624645861de4f288f8dba2865b108564d5543db
Ryan McKillen <ryan.mckillen@gmail.com>
2010-01-30 19:30:23 -0800

    basics all working

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,23 +1,30 @@
 class Alert < ActiveRecord::Base
   belongs_to :feed
-  after_create { self.send_later(:get_metrics) } 
+  after_create :get_metrics_job
   
-  def get_metrics
-    require 'cgi'
-    require 'base64'
-    require 'open-uri'
-    require 'hmac-sha1'
+  private
+  
+    def get_metrics_job
+      self.send_later(:get_metrics)
+    end
+  
+    def get_metrics
+      require 'cgi'
+      require 'base64'
+      require 'open-uri'
+      require 'hmac-sha1'
     
-    access_id = CGI::escape('member-acf8d277df')
-    secret_key = CGI::escape('cf23f577e2d6bf7b22aa23e6cbaa6158')  
-    expires = Time.now.to_i + (60 * 60 * 2)
-    api_call = 'url-metrics'
-    url = CGI::escape(self.url)
+      access_id = CGI::escape('member-acf8d277df')
+      secret_key = CGI::escape('cf23f577e2d6bf7b22aa23e6cbaa6158')  
     
-    signature = CGI::escape(Base64.encode64(HMAC::SHA1.digest(secret_key, "#{access_id}\n#{expires}")).chomp)
-    to_fetch = "http://lsapi.seomoz.com/linkscape/#{api_call}/#{url}?AccessID=#{access_id}&Expires=#{expires}&Signature=#{signature}"
-    self.metrics = JSON.parse(open(to_fetch).read)
-    self.save
-  end
+      expires = Time.now.to_i + (60 * 60 * 2)
+      url = CGI::escape(self.url.gsub(/(.*:\/\/)/, ''))
+      signature = CGI::escape(Base64.encode64(HMAC::SHA1.digest(secret_key, "#{access_id}\n#{expires}")).chomp)
+    
+      api_url = "http://lsapi.seomoz.com/linkscape/url-metrics/#{url}?AccessID=#{access_id}&Expires=#{expires}&Signature=#{signature}"
+    
+      self.metrics = JSON.parse(open(api_url).read)
+      self.save
+    end
   
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,12 @@ class Feed < ActiveRecord::Base
   has_many :crawls, :dependent => :destroy 
   has_many :alerts, :dependent => :destroy
   
-  after_create { Delayed::Job.enqueue(ParseFeed.new(self.id)) }
+  after_create :parse_feed_job
   
+  private
+  
+    def parse_feed_job
+      Delayed::Job.enqueue(ParseFeed.new(self.id))
+    end
+
 end
