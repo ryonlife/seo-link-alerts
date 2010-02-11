0e0f0c314012ae992cf0655e3f7ec350f548c58e
Ryan McKillen <rmckillen@donovan-brays-macbook-pro-2.local>
2010-02-11 14:00:54 -0800

    fixes

--- lib/parse_crawl.rb
+++ lib/parse_crawl.rb
@@ -1,17 +1,20 @@
 class ParseCrawl < Struct.new(:feed_id, :domain, :url)  
   def perform
-    # Crawl URL and parse for links to target domain (excluding nofollows), saving as alerts
+    # Crawl URL and parse for followed links to target domain, saving as alerts
     require 'open-uri'
-    doc = nil
-    begin
-      doc = open(url)
-    rescue OpenURI::Error => error
-      # error.io.status[0] # => 3xx, 4xx, or 5xx
-      raise "The request for a page at #{url} returned an error. #{error.message}"
-    end
-    doc = Nokogiri::HTML(doc)
+    # doc = nil
+    # begin
+    #   doc = open(url)
+    # rescue OpenURI::Error => error
+    #   # error.io.status[0] # => 3xx, 4xx, or 5xx
+    #   raise "The request for a page at #{url} returned an error. #{error.message}"
+    # end
+    # doc = Nokogiri::HTML(doc)
+    Rails.logger.info "Crawling #{url} for //a[contains(@href, '#{domain}')]"
+    doc = Nokogiri::HTML(open(url))
     @links = []
     doc.xpath("//a[contains(@href, '#{domain}')]").each do |link|
+      num_links += 1
       if link.attribute('rel').nil? || link.attribute('rel').text != 'nofollow'
         alert = Alert.create(:feed_id => feed_id, :url => url, :title => doc.at_css('title').text)
         break
