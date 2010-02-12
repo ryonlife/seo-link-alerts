5054418f7be819000312004a45b195d06782d6b8
Ryan McKillen <ryan.mckillen@gmail.com>
2010-02-11 22:47:12 -0800

    removed link counter

--- lib/parse_crawl.rb
+++ lib/parse_crawl.rb
@@ -14,7 +14,6 @@ class ParseCrawl < Struct.new(:feed_id, :domain, :url)
     doc = Nokogiri::HTML(open(url))
     @links = []
     doc.xpath("//a[contains(@href, '#{domain}')]").each do |link|
-      num_links += 1
       if link.attribute('rel').nil? || link.attribute('rel').text != 'nofollow'
         alert = Alert.create(:feed_id => feed_id, :url => url, :title => doc.at_css('title').text)
         break
