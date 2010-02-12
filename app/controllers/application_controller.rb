6cd75f6e75af021b3bb34b35fbe842a2db4ab7fb
Ryan McKillen <rmckillen@donovan-brays-macbook-pro-2.local>
2010-02-11 17:23:04 -0800

    domain redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # before_filter :ensure_domain
   after_filter :set_xhr_flash
   helper :all
   protect_from_forgery
@@ -8,13 +9,17 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user
   
-  protected
+  private
+  
+    def ensure_domain
+      if ENV['RAILS_ENV'] == 'development' && request.env['HTTP_HOST'] != 'la.ryonlife.com'
+        redirect_to 'http://la.ryonlife.com'
+      end
+    end
   
     def set_xhr_flash
       flash.discard if request.xhr?
-    end  
-  
-  private
+    end
   
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
