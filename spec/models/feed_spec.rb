47080098d9d88a168ad18f4dd56792dddcd593e6
Ryan McKillen <ryan.mckillen@gmail.com>
2010-02-15 13:17:02 -0800

    whatever

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe Feed do
   before(:each) do
     @valid_attributes = {
-      :url => 'http://www.google.com/alerts/feeds/15340011665024776864/13479364529815066233',
-      :domain => 'test.com'
+      :url => 'http://www.google.com/alerts/feeds/15340011665024776864/13479364529815066233'
     }
   end
 
