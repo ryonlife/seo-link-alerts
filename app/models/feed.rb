ea960e11ed381461630e7f5783b2406d225951ae
Ryan McKillen <rmckillen@donovan-brays-macbook-pro-2.local>
2010-04-19 13:12:19 -0700

    various

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,14 +6,14 @@ class Feed < ActiveRecord::Base
   
   private
   
-    def get_name
-      feed = Feedzirra::Feed.fetch_and_parse(self.url)
-      self.name = feed.title[16,feed.title.length - 16]
-      if self.name.nil? then false end
-    end
-  
-    def parse_feed_job
-      Delayed::Job.enqueue(ParseFeed.new(self.id))
-    end
+  def get_name
+    feed = Feedzirra::Feed.fetch_and_parse(self.url)
+    self.name = feed.title[16, feed.title.length - 16]
+    false if self.name.nil?
+  end
+
+  def parse_feed_job
+    Delayed::Job.enqueue(ParseFeed.new(self.id))
+  end
 
 end
